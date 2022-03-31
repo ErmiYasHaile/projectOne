12b380f22ccd0f5106f626b9a3da27b0fd9784a5
Ermiyas  Hailemichael <yemeerma11@gmail.com>
2022-03-30 18:30:13 -0700

    final update

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
-// const APIKEY =  `53e18847d94fd95d96f8426b17884ed2`
-// const $searchitem=$("input").val()
+
 const $button=$("button")
 const restcountries=(choice)=> {
     $.ajax( 
@@ -10,34 +9,17 @@ const restcountries=(choice)=> {
      console.log(data)
     console.log(data[0].flags.png)
      $('#capital').text(data[0].capital);
-    $('#country_code').text(`${data[0].idd.root}`); // to get the country code
-    $('#population').text(`${data[0].population}`); // to ge the population
-    $('#currencies').text(Object.keys(data[0].currencies)[0]); // the currencie
-    $('#region').text(data[0].region); // the region
-
-    // $img.appendedTo("img-container")
+    $('#country_code').text(`${data[0].idd.root}`); 
+    $('#population').text(`${data[0].population}`);
+    $('#currencies').text(Object.keys(data[0].currencies)[0]); 
+    $('#region').text(data[0].region); 
+    
     $("img").attr("src",data[0].flags.png)
     
 })
 }
 
-//  .catch((error)=> { 
-//      console.log(error)
-//  })
-
-// const $img = $("<img id='apod'>")
-
 $button.on("click",()=>{
     const $searchitem = $("#search").val() 
       restcountries($searchitem) 
-
 })
-// restcountries($searchitem)
-
-// `${data.data[0]}`,
-//     `${data.data[0].idd}`,
-//     `${data.data[0].countrycode}`,
-//     `${data.data[0].population}`,
-//     `${data.data[0].curriencies.Eur.name}`,
-//     `${data.data[0].content[0]}`,
-//     `${data.data[0].region}`,
